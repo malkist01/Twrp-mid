c6473aa544aa3f572394550233eaadf7e94b3d41
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-03-21 02:21:47 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,15 +17,24 @@
 #
 # Please maintain this if you use this script or any part of it
 
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+FDEVICE="mido"
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
 
-FDEVICE='mido'
-
-if ! [ "$1" = "$FDEVICE" ]; then
-    if ! [ "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-        exit 1
-    fi
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
 fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+
+ # Fox-specific flags
 
 export TW_DEFAULT_LANGUAGE='en'
 export OF_DONT_PATCH_ENCRYPTED_DEVICE='1'
